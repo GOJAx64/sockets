c116db0e24f15a2ed1af9a991fd981e9e5c5be7c
GOJABoop <josealbertogarcia453@gmail.com>
2023-06-25 00:07:26 -0600

    Changing to a express class server

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-const express = require('express'); 
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const Server = require("./models/server");
+require('dotenv').config();
 
-app.use( express.static( __dirname + '/public') );
-
-io.on('connection', ( socket ) => {
-    console.log('Conectado');
-
-    // socket.on('message-to-server', (data) => {
-    //     console.log(data);
-    // });
-
-    // socket.emit('message-from-server', { msg: 'Hello client' });
-});
-
-server.listen(8080, () => {
-    console.log('Server running at port 8080');
-})
+const server = new Server();
+server.execute();
